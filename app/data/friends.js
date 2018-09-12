85c1be955f4481d35c235bc81bb71db33f3a22a4
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-09-12 13:31:18 -0600

    friendfinder

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+
+{
+    name: "Dr. Rugferd Azzinteyer",
+    photo: "../assets/images/rugAzz.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+  },
+  {
+    name: "Alphred Integregor Varchar II",
+    photo: "../assets/images/AIV.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+  },
+  {
+    name: "Rort Hoinklehop",
+    photo: "../assets/images/rortH.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+  },
+  {
+    name: "Bobrick Plavidiar Geahmontepoti",
+    photo: "../assets/images/bobR.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+  },
+  {
+    name: "Lendrick Cashburn",
+    photo: "../assets/images/lendC.jpg",
+    scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+  },
+]
+module.exports = friends;
