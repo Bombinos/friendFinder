6a6f9258eaf0c31e11d934e46b62028211d8d5ee
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-09-13 04:42:57 -0600

    friendfinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
 
 {
     name: "Dr. Rugferd Azzinteyer",
-    photo: "../assets/images/rugAzz.jpg",
+    photo: "https://www.askideas.com/media/36/Baby-Mr-Bean-Funny-Photoshopped-Image.jpg",
     scores: [
         "1",
         "1",
@@ -18,67 +18,83 @@ var friends = [
   },
   {
     name: "Alphred Integregor Varchar II",
-    photo: "../assets/images/AIV.jpg",
+    photo: "http://www.smosh.com/wp-content/uploads/2015/06/baby-pictures-adult.jpg",
     scores: [
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1"
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
       ]
   },
   {
     name: "Rort Hoinklehop",
-    photo: "../assets/images/rortH.jpg",
+    photo: "http://larryhehn.com/wp-content/uploads/bearded-baby1.jpg",
     scores: [
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1"
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"
       ]
   },
   {
     name: "Bobrick Plavidiar Geahmontepoti",
-    photo: "../assets/images/bobR.jpg",
+    photo: "http://www.smosh.com/wp-content/uploads/2015/06/baby-picture-beard.jpg",
     scores: [
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1"
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4"
       ]
   },
   {
     name: "Lendrick Cashburn",
-    photo: "../assets/images/lendC.jpg",
+    photo: "http://www.freakingnews.com/pictures/98500/Fidel-Castro-as-a-Child--98580.jpg",
     scores: [
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1",
-        "1"
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
       ]
   },
+  {
+    name: "Gregruary Gortextilestiltskin",
+    photo: "https://i.pinimg.com/236x/55/77/67/5577679e3b41c59cd8ea60c483e0be70--boy-scouts-facial-hair.jpg",
+    scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+  }
 ]
 module.exports = friends;
